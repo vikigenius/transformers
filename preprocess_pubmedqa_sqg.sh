4eaa0b89cd49953f2260e81091e1df4535dddd71
vikigenius <v9balasu@uwaterloo.ca>
2020-09-21 16:32:53 -0400

    Fixed preprocess script for pubmedqa

--- preprocess_pubmedqa_sqg.sh
+++ preprocess_pubmedqa_sqg.sh
@@ -5,10 +5,10 @@ aws s3 cp s3://lex-labs-python-wd/ML/SQG/PubmedQA_Processed/pubmedqa_pq_pairs.ts
 cat data/pubmedqa_pq_pairs.tsv | cut -f1 > source
 cat data/pubmedqa_pq_pairs.tsv | cut -f2 > target
 
-head -n 85000 source > data/pubmedqa_sqg/train.source
-head -n 85000 target > data/pubmedqa_sqg/train.target
+head -n 138000 source > data/pubmedqa_sqg/train.source
+head -n 138000 target > data/pubmedqa_sqg/train.target
 
-tail -n 2599 data/pubmedqa_pq_pairs.tsv | awk '!seen[$1]++' > valtest
+tail -n 2199 data/pubmedqa_pq_pairs.tsv | awk '!seen[$1]++' > valtest
 
 split -n2 valtest
 
@@ -17,4 +17,4 @@ cat xaa | cut -f2 > data/pubmedqa_sqg/val.target
 cat xab | cut -f1 > data/pubmedqa_sqg/test.source
 cat xab | cut -f2 > data/pubmedqa_sqg/test.target
 
-rm xaa xab source target
+rm xaa xab source target valtest
