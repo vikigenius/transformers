a1af9f785b4d6604af38a9b61b6e7494f779d156
vikigenius <v9balasu@uwaterloo.ca>
2020-09-25 11:57:32 -0400

    Added preprocessing and finetune scripts for hqa_canlii

---
+++ examples/seq2seq/finetune_hqa_canlii_sqg.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Add parent directory to python path to access lightning_base.py
+export PYTHONPATH="../":"${PYTHONPATH}"
+
+# the proper usage is documented in the README, you need to specify data_dir, output_dir and model_name_or_path
+# run ./finetune.sh --help to see all the possible options
+python finetune.py \
+    --learning_rate=3e-5 \
+    --fp16 \
+    --fp16_opt_level=O1 \
+    --gpus 8 \
+    --do_train \
+    --n_val 100 \
+    --val_check_interval 0.5 \
+    --eval_max_gen_length 30 \
+    --eval_beams 4 \
+    --num_train_epochs 6 \
+    "$@"
---
+++ preprocess_hqa_canlii_sqg.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+mkdir -p data/hqa_canlii_sqg
+
+cat data/hqa_canlii.tsv | cut -f1 > source
+cat data/hqa_canlii.tsv | cut -f2 > target
+
+head -n 3000 source > data/hqa_canlii_sqg/train.source
+head -n 3000 target > data/hqa_canlii_sqg/train.target
+
+tail -n 68 data/hqa_canlii_pq_pairs.tsv | awk '!seen[$1]++' > valtest
+
+cat valtest | cut -f1 > data/hqa_canlii_sqg/val.source
+cat valtest | cut -f2 > data/hqa_canlii_sqg/val.target
+cat valtest | cut -f1 > data/hqa_canlii_sqg/test.source
+cat valtest | cut -f2 > data/hqa_canlii_sqg/test.target
+
+rm source target valtest
