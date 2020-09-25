9f9bde5ee379a8765ee65b5cdf99eea4a1110418
vikigenius <v9balasu@uwaterloo.ca>
2020-09-25 12:00:06 -0400

    Corrected preprocessing for hqa_canlii

--- preprocess_hqa_canlii_sqg.sh
+++ preprocess_hqa_canlii_sqg.sh
@@ -8,7 +8,7 @@ cat data/hqa_canlii.tsv | cut -f2 > target
 head -n 3000 source > data/hqa_canlii_sqg/train.source
 head -n 3000 target > data/hqa_canlii_sqg/train.target
 
-tail -n 68 data/hqa_canlii_pq_pairs.tsv | awk '!seen[$1]++' > valtest
+tail -n 68 data/hqa_canlii.tsv | awk '!seen[$1]++' > valtest
 
 cat valtest | cut -f1 > data/hqa_canlii_sqg/val.source
 cat valtest | cut -f2 > data/hqa_canlii_sqg/val.target
