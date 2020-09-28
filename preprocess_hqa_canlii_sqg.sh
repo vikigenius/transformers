1e932d35cae60c68b29fa653b385cf0bdd42dc9f
vikigenius <v9balasu@uwaterloo.ca>
2020-09-28 15:26:26 -0400

    HQA canlii removed empty fields

--- preprocess_hqa_canlii_sqg.sh
+++ preprocess_hqa_canlii_sqg.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 mkdir -p data/hqa_canlii_sqg
-
-cat data/hqa_canlii.tsv | cut -f1 > source
-cat data/hqa_canlii.tsv | cut -f2 > target
+awk '$2!=""' data/hqa_canlii.tsv > temp
+cat temp | cut -f1 > source
+cat temp | cut -f2 > target
 
 head -n 3000 source > data/hqa_canlii_sqg/train.source
 head -n 3000 target > data/hqa_canlii_sqg/train.target
 
-tail -n 68 data/hqa_canlii.tsv | awk '!seen[$1]++' > valtest
+tail -n 68 temp | awk '!seen[$1]++' > valtest
 
 cat valtest | cut -f1 > data/hqa_canlii_sqg/val.source
 cat valtest | cut -f2 > data/hqa_canlii_sqg/val.target
