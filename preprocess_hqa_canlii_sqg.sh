efcda82b044e0cf4975e7ca6218e3f6451b12a51
vikigenius <v9balasu@uwaterloo.ca>
2020-09-28 16:42:29 -0400

    Fixed preprocess script for hqa_canlii

--- preprocess_hqa_canlii_sqg.sh
+++ preprocess_hqa_canlii_sqg.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
 mkdir -p data/hqa_canlii_sqg
-awk '$2!=""' data/hqa_canlii.tsv > temp
-cat temp | cut -f1 > source
-cat temp | cut -f2 > target
+cat data/hqa_canlii.tsv | cut -f3 > source
+cat data/hqa_canlii.tsv | cut -f2 > target
 
-head -n 3000 source > data/hqa_canlii_sqg/train.source
-head -n 3000 target > data/hqa_canlii_sqg/train.target
+head -n 2900 source > data/hqa_canlii_sqg/train.source
+head -n 2900 target > data/hqa_canlii_sqg/train.target
 
-tail -n 23 temp | awk '!seen[$1]++' > valtest
+tail -n 94 data/hqa_canlii.tsv | awk -FS="\t" '!seen[$2]++' > valtest
 
-cat valtest | cut -f1 > data/hqa_canlii_sqg/val.source
+cat valtest | cut -f3 > data/hqa_canlii_sqg/val.source
 cat valtest | cut -f2 > data/hqa_canlii_sqg/val.target
-cat valtest | cut -f1 > data/hqa_canlii_sqg/test.source
+cat valtest | cut -f3 > data/hqa_canlii_sqg/test.source
 cat valtest | cut -f2 > data/hqa_canlii_sqg/test.target
 
 rm source target valtest
